7e0ce5ee55a834a40b24d6a4e349532ef2189396
DmitriyBartenev <bartenevdmitriy@bk.ru>
2022-11-13 17:12:28 +0400

    login, signup, updateProfile, forgotPassword commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,43 @@
+import React from "react";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./Dashboard";
 import Signup from "./Signup";
-import Dashboard from './Dashboard';
-import Login from './Login';
-
+import Login from "./Login";
+import PrivateRoute from "./PrivateRoute";
+import ForgotPassword from "./ForgotPassword";
+import UpdateProfile from "./UpdateProfile";
 
 function App() {
   return (
-
-      <Container className="d-flex align-items-center justify-content-center" style={{minHeight:'100vh'}}>
-        <div className="w-100" style={{maxWidth:'400px'}}>
-          <Router>
-            <AuthProvider>
-              <Routes>
-                <Route path="/" element={<Dashboard/>}/>
-                <Route path="/signup" element={<Signup/>}/>
-                <Route path="/login" element={<Login/>}/>
-              </Routes>
-            </AuthProvider>
-          </Router> 
-        </div>
-      </Container>
-  );
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route path="/" element={
+                <PrivateRoute>
+                  <Dashboard/>
+                </PrivateRoute>
+              }/>
+              <Route path="/update-profile" component={
+                <PrivateRoute>
+                  <UpdateProfile/>
+                </PrivateRoute>
+              } />
+              <Route path="/signup" component={<Signup/>} />
+              <Route path="/login" component={<Login/>} />
+              <Route path="/forgot-password" component={<ForgotPassword/>} />
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  )
 }
 
-export default App;
+export default App
